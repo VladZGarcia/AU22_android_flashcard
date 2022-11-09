9a4602aee62f6297e388a74c037238b90aef5df6
VladZGarcia <vladi.garcia@gmail.com>
2022-11-09 09:43:49 +0100

    database created

---
+++ app/src/main/java/com/example/au22_flashcard/WordDao.kt
@@ -0,0 +1,23 @@
+package com.example.au22_flashcard
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert(word: Word)
+
+    // delete
+
+
+
+    // getAllwords
+
+
+
+
+
+}
