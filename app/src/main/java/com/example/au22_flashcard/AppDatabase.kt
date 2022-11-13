0efa461fe97ad299752c2abebb258554690f94a8
VladZGarcia <vladi.garcia@gmail.com>
2022-11-13 11:34:08 +0100

    databas save funkar

---
+++ app/src/main/java/com/example/au22_flashcard/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.au22_flashcard
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+@Database(entities = [Word::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun wordDao() : WordDao
+
+
+}
---
+++ app/src/main/java/com/example/au22_flashcard/WordDao.kt
@@ -0,0 +1,27 @@
+package com.example.au22_flashcard
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface WordDao {
+
+
+
+    @Insert
+    fun insert(word: Word)
+
+    @Delete
+    fun delete(word:Word)
+
+    @Query("SELECT * FROM word_table")
+    fun getAll() : List<Word>
+
+   // @Query("SELECT * FROM word_table WHERE category LIKE :categoryName")
+   // fun findByCategory(categoryName: String) : List<Word>
+
+
+}
